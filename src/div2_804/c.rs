8fbb908a7e5d4cd81bbcf5b2ee65ee41a6915aae
tianjiaxi <tianjiaxi@bytedance.com>
2022-07-14 13:37:13 +0800

    update

--- src/div2_804/c.rs
+++ src/div2_804/c.rs
@@ -1,11 +1,12 @@
 #![allow(unused_imports)]
+#![allow(non_upper_case_globals)]
 use std::cmp::*;
 use std::collections::*;
 use std::io::{self, prelude::*};
 use std::io::{stdin, stdout, BufWriter, Write};
 use std::ops::Bound::*;
 
-const c_mod: i32 = 1e9 as i32 + 7;
+const c_mod: usize = 1e9 as usize + 7;
 
 pub fn main() {
     let stdin = io::stdin();
@@ -13,13 +14,27 @@ pub fn main() {
     let mut sc = Scanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
     let t: usize = sc.sc();
-    let mut a = vec![0; 10005];
+    let mut ini = vec![0; 100005];
+    let mut pos = vec![0; 100005];
     for _ in 0..t {
         let n: usize = sc.sc();
         for i in 0..n {
-            a[i] = sc.sc();
+            ini[i] = sc.sc();
+            pos[ini[i]] = i;
         }
-        
+        let mut l = pos[0];
+        let mut r = pos[0];
+        let mut res = 1;
+        for i in 0..n {
+            if pos[i] < l {
+                l = pos[i];
+            } else if pos[i] > r {
+                r = pos[i];
+            } else {
+                res = res * (r - l + 1 - i) % c_mod;
+            }
+        }
+        writeln!(out, "{}", res).unwrap();
     }
 }
 pub struct Scanner<B> {
