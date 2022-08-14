a250985a0aa137b197e12188de57f2a3bf41bc0e
booiris <954520770@qq.com>
2022-08-14 19:30:04 +0800

    u

---
+++ src/code/l1422_Maximum_Score_After_Splitting_a_String.rs
@@ -0,0 +1,21 @@
+#[allow(dead_code)]
+#[allow(unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+use std::ops::Bound::*;
+struct Solution;
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+#[cfg(test)]
+pub fn main() {
+    println!("res:");
+}
+
+
--- src/code/mod.rs
+++ src/code/mod.rs
@@ -3,3 +3,5 @@ pub mod l13_roman_to_integer;
 pub mod l1403_Minimum_Subsequence_in_Non_Increasing_Order;
 pub mod l623_Add_One_Row_to_Tree;
 pub mod loffer_55_I;
+pub mod l965_Univalued_Binary_Tree;
+pub mod l1422_Maximum_Score_After_Splitting_a_String;
