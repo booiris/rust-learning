a333e152bfde4446de437e35704d6375969fbc73
booiris <954520770@qq.com>
2022-08-16 23:15:57 +0800

    u

--- src/code/l1656_Design_an_Ordered_Stream.rs
+++ src/code/l1656_Design_an_Ordered_Stream.rs
@@ -15,74 +15,74 @@ macro_rules! hashmap {
          map
     }}
 }
-struct OrderedStream {
-    head: Option<Box<Node>>,
-    index: i32,
-}
+// struct OrderedStream {
+//     head: Option<Box<Node>>,
+//     index: i32,
+// }
 
-struct Node {
-    nxt: Option<Box<Node>>,
-    id: i32,
-    val: String,
-}
+// struct Node {
+//     nxt: Option<Box<Node>>,
+//     id: i32,
+//     val: String,
+// }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl OrderedStream {
-    fn new(_n: i32) -> Self {
-        OrderedStream {
-            head: Some(Box::new(Node {
-                nxt: None,
-                id: 0,
-                val: "".to_string(),
-            })),
-            index: 1,
-        }
-    }
+// /**
+//  * `&self` means the method takes an immutable reference.
+//  * If you need a mutable reference, change it to `&mut self` instead.
+//  */
+// impl OrderedStream {
+//     fn new(_n: i32) -> Self {
+//         OrderedStream {
+//             head: Some(Box::new(Node {
+//                 nxt: None,
+//                 id: 0,
+//                 val: "".to_string(),
+//             })),
+//             index: 1,
+//         }
+//     }
 
-    fn insert(&mut self, id_key: i32, value: String) -> Vec<String> {
-        let mut now = self.head.take();
-        let mut pre = now.take();
-        loop {
-            match now {
-                Some(ref x) => {
-                    if x.id > id_key {
-                        break;
-                    }
-                    pre = now.take();
-                    now = x.nxt;
-                }
-                None => break,
-            }
-        }
-        let new_node = Box::new(Node {
-            nxt: now.take(),
-            id: id_key,
-            val: value,
-        });
+//     fn insert(&mut self, id_key: i32, value: String) -> Vec<String> {
+//         let mut now = &mut self.head;
+//         let mut pre = self.head.borrow();
+//         loop {
+//             match now.take() {
+//                 Some(x) => {
+//                     if x.id > id_key {
+//                         break;
+//                     }
+//                     pre = &now;
+//                     now = now.as_mut().unwrap().nxt;
+//                 }
+//                 None => break,
+//             }
+//         }
+//         let new_node = Box::new(Node {
+//             nxt: now.take(),
+//             id: id_key,
+//             val: value,
+//         });
 
-        pre.unwrap().nxt = Some(new_node);
+//         // pre.unwrap().nxt = Some(new_node);
 
-        let res = vec![];
-        // now = self.head.unwrap().nxt;
-        // loop {
-        //     match now {
-        //         Some(x) => {
-        //             if x.id == self.index {
-        //                 res.push(x.val);
-        //                 self.index += 1;
-        //             }
-        //             now = x.nxt;
-        //         }
-        //         None => break,
-        //     }
-        // }
+//         let res = vec![];
+//         // now = self.head.unwrap().nxt;
+//         // loop {
+//         //     match now {
+//         //         Some(x) => {
+//         //             if x.id == self.index {
+//         //                 res.push(x.val);
+//         //                 self.index += 1;
+//         //             }
+//         //             now = x.nxt;
+//         //         }
+//         //         None => break,
+//         //     }
+//         // }
 
-        res
-    }
-}
+//         res
+//     }
+// }
 
 /**
  * Your OrderedStream object will be instantiated and called as such:
@@ -90,9 +90,37 @@ impl OrderedStream {
  * let ret_1: Vec<String> = obj.insert(idKey, value);
  */
 
+struct OrderedStream {
+    data: Vec<Option<String>>,
+    index: usize,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl OrderedStream {
+    fn new(n: i32) -> Self {
+        OrderedStream {
+            data: vec![None; (n + 2) as usize],
+            index: 1,
+        }
+    }
+
+    fn insert(&mut self, mut id_key: i32, value: String) -> Vec<String> {
+        self.data[id_key as usize] = Some(value);
+        let mut res = vec![];
+        while let Some(ref x) = self.data[self.index] {
+            res.push(x.to_string());
+            self.index += 1;
+        }
+        res
+    }
+}
+
 #[cfg(feature = "local")]
 pub fn main() {
-    let obj = OrderedStream::new(5);
+    let mut obj = OrderedStream::new(5);
     let ret_1: Vec<String> = obj.insert(3, "c".to_string());
     let ret_2: Vec<String> = obj.insert(1, "a".to_string());
     let ret_3: Vec<String> = obj.insert(2, "b".to_string());
