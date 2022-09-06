35c1f47a82aece536323f84137cf51ef2d0f33b0
booiris <954520770@qq.com>
2022-09-06 16:10:07 +0800

    u

--- src/code/l_828_Count_Unique_Characters_of_All_Substrings_of_a_Given_String.rs
+++ src/code/l_828_Count_Unique_Characters_of_All_Substrings_of_a_Given_String.rs
@@ -14,10 +14,30 @@ macro_rules! hashmap {
 }
 
 impl Solution {
-    pub fn unique_letter_string(s: String) -> i32 {}
+    pub fn unique_letter_string(s: String) -> i32 {
+        let mut key = vec![[-1, s.len() as i32]; s.len()];
+        for c in 'A'..='Z' {
+            let mut pre: i32 = -1;
+            for (index, x) in s.chars().enumerate() {
+                if x == c {
+                    key[index][0] = pre;
+                    if pre != -1 {
+                        key[pre as usize][1] = index as i32;
+                    }
+                    pre = index as i32;
+                }
+            }
+        }
+        let mut res = 0;
+        for (index, _) in s.chars().enumerate() {
+            res += (index as i32 - key[index][0]) * (key[index][1] - index as i32);
+        }
+        res
+    }
 }
 
 #[cfg(feature = "local")]
 pub fn main() {
-    println!("res:");
+    let a = "IECIYJSQHMDHQPCOTCQTVYEQMEYGGVPBUPKVHAAGBQKAQQVMWTMZZSEGTYWTBCN";
+    println!("res:{}", Solution::unique_letter_string(a.to_string()));
 }
