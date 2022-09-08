203ad987ab69b8ee1d6774e802415e619c2699c7
booiris <954520770@qq.com>
2022-09-08 16:52:24 +0800

    u

---
+++ src/contest/temp/c.rs
@@ -0,0 +1,21 @@
+#![allow(dead_code, unused_imports, unused_macros)]
+use std::cmp::*;
+use std::collections::*;
+use std::ops::Bound::*;
+#[cfg(feature = "local")]
+struct Solution;
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+#[cfg(feature = "local")]
+pub fn main() {
+    println!("res:");
+}
+
+
