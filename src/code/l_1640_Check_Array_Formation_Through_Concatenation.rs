b642b96f342e208de6651bffe52e849145bc26cd
tianjiaxi <tianjiaxi@bytedance.com>
2022-09-22 13:09:06 +0800

    u

---
+++ src/code/l_1640_Check_Array_Formation_Through_Concatenation.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code, unused_imports, unused_macros)]
+use std::cmp::*;
+use std::collections::*;
+use std::ops::Bound::*;
+#[cfg(feature = "local")]
+struct Solution;
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+impl Solution {
+    pub fn can_form_array(arr: Vec<i32>, pieces: Vec<Vec<i32>>) -> bool {
+        let mut key = HashMap::new();
+        arr.iter().enumerate().for_each(|(i, &x)| {
+            key.insert(x, i);
+        });
+        for x in pieces {
+            let mut pre = -1;
+            for y in x {
+                if pre != -1 {
+                    if let (Some(z1), Some(z2)) = (key.get(&pre), key.get(&y)) {
+                        if z1 + 1 != *z2 {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else if key.get(&y).is_none() {
+                    return false;
+                }
+                pre = y;
+            }
+        }
+        true
+    }
+}
+
+#[cfg(feature = "local")]
+pub fn main() {
+    println!("res:");
+}
