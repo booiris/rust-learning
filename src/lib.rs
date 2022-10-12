98e1135a3543c1ca5527684ce6bbdddff968cacb
booiris <954520770@qq.com>
2022-10-12 17:27:21 +0800

    u

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,28 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+// macro_rules! log {
+//     ( $( $t:tt )* ) => {
+//         web_sys::console::log_1(&format!( $( $t )* ).into());
+//     }
+// }
+
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
     Dead = 0,
     Alive = 1,
+}
+
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
 }
 
 #[wasm_bindgen]
@@ -44,6 +60,17 @@ impl Universe {
         }
         count
     }
+
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells {
+            let idx = self.get_index(*row, *col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -71,19 +98,30 @@ impl Universe {
                 };
 
                 next[idx] = next_cell;
+
+                // log!(
+                //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                //     row,
+                //     col,
+                //     cell,
+                //     live_neighbors
+                // );
             }
         }
+
+        // log!("    it becomes {:?}", next);
 
         self.cells = next;
     }
 
     pub fn new() -> Universe {
-        let width = 120;
-        let height = 120;
+        utils::set_panic_hook();
+        let width = 90;
+        let height = 90;
 
         let cells = (0..width * height)
             .map(|_| {
-                if js_sys::Math::random() < 0.1 {
+                if js_sys::Math::random() < 0.2 {
                     Cell::Alive
                 } else {
                     Cell::Dead
@@ -99,14 +137,42 @@ impl Universe {
     }
 
     pub fn width(&self) -> u32 {
-        self.width
+        return self.width;
     }
 
     pub fn height(&self) -> u32 {
-        self.height
+        return self.height;
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
+    }
+
+    pub fn restart(&mut self) {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                if js_sys::Math::random() < 0.2 {
+                    self.cells[idx] = Cell::Alive;
+                } else {
+                    self.cells[idx] = Cell::Dead
+                }
+            }
+        }
     }
 }
