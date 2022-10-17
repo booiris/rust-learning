8b3a7cc47edae6ac7cf0ed6910c77919a8dc3bec
booiris <954520770@qq.com>
2022-10-17 21:47:58 +0800

    u

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,4 +1,4 @@
-import { initSync } from "./screeps-arena-starter-rust";
-import wasm_bytes from "./screeps-arena-starter-rust_bg.wasm.bin";
+import { initSync } from "./rust-screeps";
+import wasm_bytes from "./rust-screeps_bg.wasm.bin";
 initSync(wasm_bytes);
-export * from "./screeps-arena-starter-rust";
+export * from "./rust-screeps";
