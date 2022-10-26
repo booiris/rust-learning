e89027e0b26b009fd108054729b67333cc2a9f31
booiris <954520770@qq.com>
2022-10-26 20:09:06 +0800

    u

---
+++ A_Freestanding_Rust_Binary/src/main.rs
@@ -0,0 +1,16 @@
+#![allow(non_snake_case)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
