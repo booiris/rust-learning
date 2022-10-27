c194974e1767f6d0cd17fe06a88aaec522575306
booiris <954520770@qq.com>
2022-10-27 19:33:53 +0800

    u

--- Hardware_Interrupts/src/main.rs
+++ Hardware_Interrupts/src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod vga_buffer;
 use core::panic::PanicInfo;
+
+mod serial;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -11,20 +14,44 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[no_mangle]
-pub extern "C" fn _start() {
-    println!("Hello World");
-    panic!("Some panic message");
-    println!("Hello World!");
-    println!("game time!");
+pub extern "C" fn _start() -> ! {
+    serial_println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
