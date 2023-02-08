076106f776e3ff3f7a758473c805a8b74b10cbc2
booiris <954520770@qq.com>
2023-02-08 13:05:27 +0800

    u

---
+++ src/code/l_1233_Remove_Sub-Folders_from_the_Filesystem.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code, unused_imports, unused_macros)]
+use std::cmp::*;
+use std::collections::*;
+use std::ops::Bound::*;
+#[cfg(feature = "local")]
+struct Solution;
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+
+impl Solution {
+    pub fn remove_subfolders(folder: Vec<String>) -> Vec<String> {
+
+    }
+}
+
+#[cfg(feature = "local")]
+pub fn main() {
+    println!("res:");
+}
+
+
--- src/code/mod.rs
+++ src/code/mod.rs
@@ -140,3 +140,4 @@ pub mod l_2293_Min_Max_Game;
 pub mod l_2325_Decode_the_Message;
 pub mod l_1129_Shortest_Path_with_Alternating_Colors;
 pub mod l_2331_Evaluate_Boolean_Binary_Tree;
+pub mod l_1233_Remove_Sub-Folders_from_the_Filesystem;
