e6b64a5b1d29c81ba2615f5238a8a85919907596
booiris <954520770@qq.com>
2023-05-03 00:19:57 +0800

    Wed May  3 12:19:57 AM CST 2023

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -155,3 +155,4 @@ pub mod l_1027_Longest_Arithmetic_Subsequence;
 pub mod l_1031_Maximum_Sum_of_Two_Non_Overlapping_Subarrays;
 pub mod l_1172_Dinner_Plate_Stacks;
 pub mod l_2423_Remove_Letter_To_Equalize_Frequency;
+pub mod l_1003_Check_If_Word_Is_Valid_After_Substitutions;
