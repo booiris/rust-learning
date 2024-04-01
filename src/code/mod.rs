61aa111c2dcf0a5d5a4124c4f2ebad4d72a18762
booiris <booiris02@gmail.com>
2024-04-01 22:27:43 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -199,3 +199,4 @@ pub mod l_1997_First_Day_Where_You_Have_Been_in_All_the_Rooms;
 pub mod l_2908_Minimum_Sum_of_Mountain_Triplets_I;
 pub mod l_2952_Minimum_Number_of_Coins_to_be_Added;
 pub mod l_331_Verify_Preorder_Serialization_of_a_Binary_Tree;
+pub mod l_2810_Faulty_Keyboard;
