c2c9ee09b03bb9cf6febc014a45a3d7553b88bd7
booiris <booiris02@gmail.com>
2025-08-12 21:49:07 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -241,3 +241,4 @@ pub mod l_118_Pascals_Triangle;
 pub mod l_2561_Rearranging_Fruits;
 pub mod l_2106_Maximum_Fruits_Harvested_After_at_Most_K_Steps;
 pub mod l_3479_Fruits_Into_Baskets_III;
+pub mod l_2787_Ways_to_Express_an_Integer_as_Sum_of_Powers;
