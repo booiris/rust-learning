d1d5accd83fd0b4719e2fde2b44ff241a2353ea4
booiris <booiris02@gmail.com>
2025-08-22 19:01:50 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -247,3 +247,4 @@ pub mod l_1780_Check_if_Number_is_a_Sum_of_Powers_of_Three;
 pub mod l_1323_Maximum_69_Number;
 pub mod l_2348_Number_of_Zero_Filled_Subarrays;
 pub mod l_1277_Count_Square_Submatrices_with_All_Ones;
+pub mod l_3195_Find_the_Minimum_Area_to_Cover_All_Ones_I;
