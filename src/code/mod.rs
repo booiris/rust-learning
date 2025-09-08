b25ec3c1a741639cfb4225b14ef56cca038e03b6
booiris <booiris02@gmail.com>
2025-09-08 21:38:03 +0800

    update build ci

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -255,3 +255,4 @@ pub mod l_3025_Find_the_Number_of_Ways_to_Place_People_I;
 pub mod l_3516_Find_Closest_Person;
 pub mod l_3495_Minimum_Operations_to_Make_Array_Elements_Zero;
 pub mod l_1304_Find_N_Unique_Integers_Sum_up_to_Zero;
+pub mod l_1317_Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers;
