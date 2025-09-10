70c2801279f6cf8fcc922dcfe2961feb4cec4286
booiris <booiris02@gmail.com>
2025-09-10 23:20:07 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -256,3 +256,4 @@ pub mod l_3516_Find_Closest_Person;
 pub mod l_3495_Minimum_Operations_to_Make_Array_Elements_Zero;
 pub mod l_1304_Find_N_Unique_Integers_Sum_up_to_Zero;
 pub mod l_1317_Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers;
+pub mod l_1733_Minimum_Number_of_People_to_Teach;
