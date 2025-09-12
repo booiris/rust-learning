54fb6c5f69782d7c51d5e621f52f6e37d98b39a3
booiris <booiris02@gmail.com>
2025-09-12 10:10:56 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -258,3 +258,4 @@ pub mod l_1304_Find_N_Unique_Integers_Sum_up_to_Zero;
 pub mod l_1317_Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers;
 pub mod l_1733_Minimum_Number_of_People_to_Teach;
 pub mod l_2785_Sort_Vowels_in_a_String;
+pub mod l_3227_Vowels_Game_in_a_String;
