58409f5edc312cdb76d8e9514c511bf76e3d606c
booiris <booiris02@gmail.com>
2025-09-21 10:37:16 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -263,3 +263,5 @@ pub mod l_3541_Find_Most_Frequent_Vowel_and_Consonant;
 pub mod l_2349_Design_a_Number_Container_System;
 pub mod l_3408_Design_Task_Manager;
 pub mod l_3484_Design_Spreadsheet;
+pub mod l_1912_Design_Movie_Rental_System;
+pub mod l_3508_Implement_Router;
