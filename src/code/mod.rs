c2f0a17097d82c7f9876008fac02d109199d21ab
booiris <booiris02@gmail.com>
2025-09-28 21:02:17 +0800

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -271,3 +271,4 @@ pub mod l_166_Fraction_to_Recurring_Decimal;
 pub mod l_120_Triangle;
 pub mod l_611_Valid_Triangle_Number;
 pub mod l_812_Largest_Triangle_Area;
+pub mod l_976_Largest_Perimeter_Triangle;
