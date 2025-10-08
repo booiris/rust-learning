5cee80315907a48fbffc183ec7e49c84f52abf7f
booiris <booiris02@gmail.com>
2025-10-08 02:01:02 +0000

    update

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -281,3 +281,4 @@ pub mod l_11_Container_With_Most_Water;
 pub mod l_417_Pacific_Atlantic_Water_Flow;
 pub mod l_778_Swim_in_Rising_Water;
 pub mod l_1488_Avoid_Flood_in_The_City;
+pub mod l_2300_Successful_Pairs_of_Spells_and_Potions;
